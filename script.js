d86800dc5df7227f33789959f836dfcfd09c0ab1
Jason Mar <jcmar@cpp.edu>
2023-10-06 17:18:25 -0700

    setting up repo and first files

---
+++ script.js
@@ -0,0 +1,6 @@
+// const buttons = document.querySelectorAll('button:not(operand)');
+// //('.ethan+button) = grab button ethan then one right next to it, only the sibling will work
+    
+// for (let button of buttons){
+//     button.addEventListener('click', changeColor)
+// }
